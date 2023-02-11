66bb8e200c8fc10bbccf281842674aac49a9b40c
Ozosociety <125140489+Ozosociety@users.noreply.github.com>
2023-02-11 23:44:46 +0530

    Add files via upload

---
+++ assets/js/eventInfoData.js
@@ -0,0 +1,60 @@
+let eventInfoData = [
+    {
+        delay: "100",
+        class: "bxl-dribbble",
+        name: "QR KAA WAR",
+        info: "SOCIETY organize a special war in the college ,Under this participants have to scn QR code to find next destination. AS it is Annual Event, the mystery changes every year but the prinnciples remain same wich makes it more intresting. AS it is team work then top 3 teams which cross all hurdles are awarded."
+    },
+    {
+        delay: "200",
+        class: "bx-file",
+        name: "Workshops on Basics of Computer",
+        info: "Society conducted a workshop on THE BASICS OF COMPUTER FOR FIRST AND SECOND YEAR students of the institute. We covered topics like different applications of Microsoft Package (mainly WORD ,POWERPOINT , EXCEL) ; DBMS , MS ACCESS ; COMPUTER ARCHITECTURE , CLASSIFICATION AND GENERATIONS. Event was conducted at IT CONFERENCE HALL, IT BLOCK."
+    },
+    {
+        delay: "100",
+        class: "bx-game",
+        name: "Blur Racing",
+        info: "Society organised a gaming event. The event was a great success as we got participation of 53 students from different branches.The winner and the runner up of the event were awarded with prizes along with CERTIFICATES from the Society."
+    },
+    {
+        delay: "300",
+        class: "bx-arch",
+        name: "Excursion to Mussorie",
+        info: "OZONE SOCIETY organised a trip to the queen of hills,Mussorie.It was a THREE DAY excrusion along with a group of 30 STUDENTS and tour was open for every individual of the institute . Students were accompanied by 2 faculty members..DR. NIDHI SHARMA AND MR.HARDIK CHAUDHRY. A special thanks to DR. MUKESH KUMAR it would not be possible without his support. All the arrangement were made prior in hand and the society looks forward to plan more trips in future."
+    },
+    {
+        delay: "300",
+        class: "bx-file",
+        name: "Workshop on Adobe PhotoShop, Canva, C Basics",
+        info: "Society organised a workshop for the FIRST AND SECOND YEAR CS-IT STUDENTS of the institute on some of the advanced topics of computer,i.e. ADOBE PHOTOSHOP,CANVA AND BASICS OF C LANGUAGE."
+    },
+    {
+        delay: "300",
+        class: "bx-arch",
+        name: "DART HTC",
+        info: "Annual event of Society, DART-HTC Is A QUIZ organised by society to test IQ level of freshers. The major temptation is Graphics Round in which different type of sub-round take place."
+    },
+    {
+        delay: "300",
+        class: "bx-arch",
+        name: "OPEN SOURCE CLASS",
+        info: "Society organised open classes in 2016-17 under guidance of Mr. Surender Singh. In The classes, we teach open source technologies starting with linux, Virtual box, creating and running virtuamachines and various open source software. Through this students got technical information about how to use open source software. Overall those classes were very knowledgable."
+    },
+    {
+        delay: "300",
+        class: "bx-arch",
+        name: "External Workshop",
+        info: "GOV.SR.SEC SCHOOL , SISAR KHAS MEHAM Society Organised a workshop for School Students om NETWORKING, OSI(Open Source Interconnection), TOPOLOGIES, applications of Microsoft Package."
+    },
+    {
+        delay: "300",
+        class: "bx-arch",
+        name: "Web Development and Hosting",
+        info: "Society organised a workshop on WEB DEVELOPMENT AND HOSTING in (2016-17).It was an intra society event.WE DEVELOPED AND HOSTED OUR OWN OZONE WEBSITE. Website created was launced in another event which was open for everyone to attend.Main motive of the event was to provide experience of website creation."
+    }
+
+];
+
+
+export {eventInfoData};
---
+++ assets/js/membersData.js
@@ -0,0 +1,610 @@
+export const membersPre = [
+  {
+    name: "Gourav",
+    cid: "20CE096",
+    fb: "gouravgrover298@gmail.com",
+    ln: "https://www.linkedin.com/in/gourav-02a82a200",
+    image: "Gaurav.jpg"
+      },
+      {
+        name: "Suhani",
+        cid: "20EC011",
+        fb: "pulkitgarf@gmail.com",
+        ln: "https://www.linkedin.com/in/suhani-yadav-06035b200",
+        image: "Suhani.jpg"
+          },
+          {
+            name: "Dhruv Verma ",
+            cid: "20CE094",
+            fb: "vermadhruv09112002@gmail.com",
+            ln: "https://www.linkedin.com/in/dhruv-verma-3b2110209",
+            image: "DhruvVerma.jpg"
+            },  
+      
+      
+
+     
+      {
+        name: "Vanshika ",
+        cid: "20CE027",
+        fb: "vanshikakakkar28@gmail.com",
+        ln: "https://www.linkedin.com/in/vanshika-kakkar-052a84211",
+        image: "VanshikaKakkar.jpg",
+        },
+        {
+          name: "Harshita Gupta",
+          cid: "20ce103",
+          fb: "harshitagupta933@gmail.com",
+          ln: "https://www.linkedin.com/in/harshita-gupta-889278205",
+          image: "HarshitaGupta.jpg",
+          },
+        
+          {
+          name: "Jatin Verma",
+          cid: "20EC023",
+          fb: "verma9650gmail.com",
+          ln: "https://www.linkedin.com/in/jatin-verma-58a4a5205",
+          image: "JaiVerma.jpg",
+          },
+    
+      
+     
+      {
+    name: "Mudit",
+    cid: "20TC011",
+    fb: "muditraheja11@gmail.com",
+    ln: "https://www.linkedin.com/in/mudit-raheja-91b201200",
+    image: "MuditRaheja.png"
+      },
+    
+     
+      {
+    name: "Khushi Aggarwal",
+    cid: "20CE024",
+    fb: "khushiagg501@gmail.com",
+    ln: "https://www.linkedin.com/in/khushi-a-0b935a200",
+    image: "KhushiAggarwal.jpg"
+      },
+      {
+    name: "Pranav",
+    cid: "20CE028",
+    fb: "Pranavkamra9798@gmail.com",
+    ln: "https://www.linkedin.com/in/pranav-kamra-0a9771201",
+    image: "PranavKamra.png"
+      },
+      {
+    name: "Sahiba",
+    cid: "20CE063",
+    fb: "sahibamidha3030@gmail.com",
+    ln: "https://www.linkedin.com/in/sahiba-midha-253a21208",
+    image: "Sahibamidha.jpg"
+      },
+    
+      {
+    name: "Pratham soni",
+    cid: "20CE090",
+    fb: "Prathamsoni81830@gmail.com",
+    ln: "https://www.linkedin.com/in/pratham-soni-1a58a5210",
+    image: "PrathamSoni.jpg"
+      },
+      {
+        name: "Mukul",
+        cid: "20ce048",
+        fb: "mukulattri69@gmail.com",
+        ln: "https://www.linkedin.com/in/mukul-attri-8038a3200",
+        image: "MukulAttri.jpg"
+          },
+          {
+            name: "Manisha",
+            cid: "20CE050",
+            fb: "manisha15720@gmail.com",
+            ln: "https://www.linkedin.com/in/manisha-400537200",
+            image: "Manisha.jpg"
+              },
+     
+      {
+        name: "Bharat",
+        cid: "20CE022",
+        fb: "khattershab@gmail.com",
+        ln: "https://www.linkedin.com/in/bharat-b222771b2",
+        image: "Bharat.jpg"
+          },   
+          {
+            name: "Mohita",
+            cid: "20CE051",
+            fb: "mohita3022@gmail.com",
+            ln: "https://www.linkedin.com/in/mohita-69836a200",
+            image: "Mohita.png"
+              },
+                
+        {
+        
+        name: "Ishita",
+        cid: "20CE087",
+        fb: "kaushik.ishita001@gmail.com",
+        ln: "https://www.linkedin.com/in/ishita-kaushik-168aa9211",
+        image: "IshitaKaushik.jpg"
+        },
+        {
+        name: "Tushar",
+        cid: "20ba035",
+        fb: "tusharsinghlegha@gmail.com",
+        ln: "https://www.linkedin.com/in/tushar-singh-6039a2206",
+        image: "TusharGirdhar.jpg"
+        },
+     
+        {
+       
+        name: "Karishma bansal",
+        cid: "20ce091",
+        fb: "karishma17102003@gmail.com",
+        ln: "https://www.linkedin.com/in/karishma-bansal-818683211",
+        image: "karishma.jpeg"
+        },
+      {
+      name: "Tushar ",
+      cid: "20ba035",
+      fb: "tusharsinghlegha@gmail.com",
+      ln: "https://www.linkedin.com/in/tushar-singh-6039a2206",
+      image: "TusharSingh.jpg",
+      },
+   
+      {
+    name: "Vishal Singh",
+    cid: "20TT004",
+    fb: "vishalsingh310703@gmail.com",
+    image: "VishalSingh.jpg"
+      },
+      {
+      name: "Nidhi",
+      cid: "20fa201",
+      fb: "nkukreja24902@gmail.com",
+      image: "NiDHiKuKrEjA.jpg",
+      },
+     
+      {
+    name: "Nisha",
+    cid: "20CE097",
+    fb: "Janagalnisha03@gmail.com",
+    image: "nisha.jpeg"
+      },
+]
+
+export const membersFinal = [
+  {
+    name: "Purvi",
+    cid: "19CE084",
+    fb: "purvimunjal2002@gmail.com",
+    ln: "https://www.linkedin.com/in/purvi-munjal-8890a31b3",
+    image: "purvi.jpeg"
+      },
+      {
+    name: "Vinay",
+    cid: "19CE105",
+    fb: "vinaykumarsinghal0709@gmail.com",
+    ln: "https://www.linkedin.com/in/vinay-kumar-5931131a3",
+    image: "vinay.jpg"
+      },
+      {
+    name: "Moksh Rohilla",
+    cid: "19TT022",
+    fb: "mokshrohilla2801@gmail.com",
+    ln: "https://www.linkedin.com/in/moksh-rohilla-669791191",
+    image: "MokshRohilla.jpg"
+      },
+      {
+    name: "Jayanti Jha",
+    cid: "19FA006",
+    fb: "jayantijha23fae@gmail.com",
+    ln: "https://www.linkedin.com/in/jayanti-jha-36243218b",
+    image: "JayantiJha.JPG"
+      },
+ 
+      {
+    name: "Kirti",
+    cid: "19CE053",
+    fb: "kirtimittal.in@gmail.com",
+    ln: "https://www.linkedin.com/in/kirti-mittal-a861131a3",
+    image: "kirtimittal.jpg"
+      },
+      {
+    name: "Apsara",
+    cid: "19CE018",
+    fb: "apsararaheja@gmail.com",
+    ln: "https://www.linkedin.com/in/apsara-raheja-b77b41199",
+    image: "ApsaraRAheja.jpg"
+      },
+      {
+    name: "Kanika",
+    cid: "19CE049",
+    fb: "kanikarohilla3@gmail.com",
+    ln: "https://www.linkedin.com/in/kanika-rohilla-b0a1131a3",
+    image: "kanika.jpeg"
+      },
+      {
+    name: "Rahul",
+    cid: "19CE085",
+    fb: "rahulkumarroy723@gmail.com",
+    ln: "https://www.linkedin.com/in/rahul-kumar-56b700165",
+    image: "rahul.jpeg"
+      },
+      {
+    name: "Neha",
+    cid: "19CE073",
+    fb: "nehamakhija816@gmail.com",
+    ln: "https://www.linkedin.com/in/neha-makhija-3559071aa",
+    image: "neha.jpeg"
+      },
+   
+      {
+    name: "Nikhil Singh",
+    cid: "19CE075",
+    fb: "nikhilhoney.2000@gmail.com",
+    ln: "https://www.linkedin.com/in/nikhil-singh-357b541a6",
+    image: "NikhilSingh.jpg"
+      },
+      {
+    name: "Preeti Maurya",
+    cid: "19CE082",
+    fb: "preetimaurya188@gmail.com",
+    ln: "https://www.linkedin.com/in/preeti-maurya-461b691a9",
+    image: "PREETIMAURYA.jpeg"
+      },
+    
+      {
+    name: "Pratham",
+    cid: "19CE081",
+    fb: "upratham07@gmail.com",
+    ln: "https://www.linkedin.com/in/pratham-uttreja-b071131a3",
+    image: "prathamuttreja.jpg"
+      },
+      {
+    name: "Chaitanya",
+    cid: "19CE022",
+    fb: "Chaitanyaasit@gmail.com",
+    ln: "https://www.linkedin.com/in/chaitanya-asit-8520b3192",
+    image: "chaitanyaasit.jpg"
+      },
+      {
+    name: "Abhishek Gupta",
+    cid: "19tt002",
+    fb: "abhishek13122001@gmail.com",
+    ln: "https://www.linkedin.com/in/abhishek-gupta-639221192",
+    image: "AbhishekGupta.jpg"
+      },
+      {
+    name: "Lovish Kumar",
+    cid: "19CE060",
+    fb: "lovishtuteja506@gmail.com",
+    ln: "https://www.linkedin.com/in/lovish-tuteja-b63a941a8",
+    image: "LOVISHTUTEJA.jpeg"
+      },
+     
+      {
+    name: "Mandeep",
+    cid: "19CE061",
+    fb: "mandeepses132@gmail.com",
+    ln: "https://www.linkedin.com/in/vivansingh-mandeep",
+    image: "Vivansingh.jpg"
+      },
+      {
+    name: "Shukla Gaurav",
+    cid: "19TC103",
+    fb: "gshukla695@gmail.com",
+    image: "GauravShukla.jpg"
+      },
+    
+      {
+    name: "Sneha Goyal",
+    cid: "19CE098",
+    fb: "sngyl34@gmail.com",
+    ln: "https://www.linkedin.com/in/sneha-goyal-a40502201",
+    image: "SnehaGoyal.jpg"
+      },
+      
+]
+
+export const membersSecond = [
+  {
+    name: "Saksham ",
+    cid: "21CE026",
+    fb: "sakshamdudeja1@gmail.com",
+    image: "SakshamDudeja.jpg",
+    },
+    {
+    name: "Abhishek ",
+    cid: "21CE085",
+    fb: "abg98128@gmail.com",
+    ln: "https://www.linkedin.com/in/abhishek-210950233",
+    image: "Abhishek.jpg",
+    },
+    {
+   
+      name: "Sanya",
+      cid: "21CE100",
+      fb: "sanyabajaj2003@gmail.com",
+      image: "SanyaBajaj.jpg"
+      },
+      {
+     
+      name: "Lavisha",
+      cid: "21CE069",
+      fb: "lavisha985@gmail.com",
+      image: "LavishaMehta.jpeg"
+      },
+      {
+        name: "Chirag",
+        cid: "21CE006",
+        fb: "chiragmehtani9@gmail.com",
+        image: "ChiragMehtani.jpg"
+        },
+        {
+        name: "Gaurav Kumar Sangwan",
+        cid: "21CE021",
+        fb: "tigersangwan1@gmail.com",
+        image: "GaUraVSanGwaN.jpg"
+        },
+    {
+      name: "Shivam",
+      cid: "21CE050",
+      fb: "shivammunjal761@gmail.com",
+      image: "ShivamMunjal.jpg"
+      },
+      {
+      name: "Gautam",
+      cid: "21CE055",
+      fb: "gautamvyas911@gmail.com",
+      image: "GautamVyas.jpg"
+      },
+      {
+      name: "Kashish",
+      cid: "21BA053",
+      fb: "mukhijakashish73@gmail.com",
+      ln: "Kashish Mukhija",
+      image: "KashishMukhija.jpg"
+      },
+      {
+      name: "Neeraj",
+      cid: "21CE053",
+      fb: "beriwalneeraj2468@gmail.com",
+      ln: "https://www.linkedin.com/in/neeraj-beriwal-89296322a/",
+      image: "NeerajBeriwal.jpeg"
+      },
+      {
+      name: "Aryan",
+      cid: "21CE013",
+      fb: "Sardanaaryan40@gmail.com",
+      image: "AryanSardana.jpg"
+      },
+    {
+      name: "Hitasha",
+      cid: "21CE016",
+      fb: "hitasha922@gmail.com",
+      image: "HitashaVerma.jpg"
+      },
+      {
+      
+      name: "Kavya Gakhar",
+      cid: "21CE011",
+      fb: "Itskavyagakhar@gmail.com",
+      ln: "https://www.linkedin.com/in/itskavyagakhar7",
+      image: "KavyaGakhar.jpg"
+      },
+      {
+      name: "Priyanshi",
+      cid: "21CE080",
+      fb: "priyanshi15042003@gmail.com",
+      ln: "https://www.linkedin.com/in/priyanshi-25a984229",
+      image: "Priyanshi.jpeg"
+      },
+      {
+ 
+        name: "Joy",
+        cid: "21ec013",
+        fb: "aryanriegn24003@gmail.com",
+        image: "JoyJuneja.jpg"
+        },
+       
+        {
+       
+        name: "Naman",
+        cid: "21CE082",
+        fb: "namantohilla11607@gmail.com",
+        image: "NamanRohilla.jpg"
+        },
+        {
+        name: "Kirti ",
+        cid: "21BA055",
+        fb: "kirtibansal1411@gmail.com",
+        image: "KirtiBansal.jpg",
+        },
+        {
+        name: "Manisha Kukreja",
+        cid: "21Ce096",
+        fb: "manishakukreja99999@gmail.com",
+        image: "ManishaKukreja.jpg"
+        },
+        {
+          name: "Hardik",
+          cid: "21CE010",
+          fb: "hardik2003chhabra@gmail.com",
+          image: "HardikChabhra.jpg"
+          },
+       
+          {
+        
+          name: "Raghav",
+          cid: "21CE005",
+          fb: "mehtaraghav60@gmail.com",
+          image: "RaghavMehta.jpg"
+          },
+          {
+          name: "Lakshita Verma",
+          cid: "21CE004",
+          fb: "lkv0390@gmail.com",
+          image: "LakshitaVerma.jpg"
+          },
+          {
+          name: "Sanchi",
+          cid: "21BA054",
+          fb: "Sanchimehta70@gmail.com",
+          image: "sanchiMehta.jpg"
+          },
+]
+
+export const membersFirst = [
+  {
+   
+    name: "Yashika",
+    cid: "22CE037",
+    fb: "yashikaaggarwal8607@gmail.com",
+    image: "Yashika.jpg",
+    },
+    {
+ 
+    name: "Anshu",
+    cid: "22EC002",
+    fb: "anshusangwan2004@gmail.com",
+    ln: "Anshu Sangwan",
+    image: "AnshuSangwan.jpg",
+    },
+    {
+      name: "Jivesh",
+      cid: "22CE073",
+      fb: "makhijajivesh@gmail.com",
+      ln: "https://www.linkedin.com/in/jivesh-d%C3%AE%C3%B1%C3%B8d%C3%AE%C3%A5-094497256",
+      image: "JiveshDîñødîå.jpg",
+      },
+     
+      {
+  
+      name: "Aman aggarwal",
+      cid: "22ba004",
+      fb: "Amanaggarwal8950@gmail.com",
+      image: "AmanAggarwal.jpg"
+      },
+    {
+      name: "Shivam sharma",
+      cid: "22CE145",
+      fb: "kaushikshivam540@gmail.com",
+      ln: "https://www.linkedin.com/in/shivam-kaushik-2a7406248",
+      image: "ShivamKaushik.jpg",
+      },
+      {
+ 
+        name: "Anshika",
+        cid: "22CE133",
+        fb: "anshikarakheja1@gmail.com",
+        image: "AnshikaRakheja.jpg"
+        },
+        {
+       
+        name: "Kanan garg",
+        cid: "22ba043",
+        fb: "kanangarg16405@gmail.com",
+        image: "KananGarg.jpg"
+        },
+        {
+      
+        name: "Dilip",
+        cid: "22CE038",
+        fb: "dilipsharmaa385@gmail.com",
+        image: "DÌLÍP.jpg"
+        },
+      {
+      name: "Chanchal",
+      cid: "22BA027",
+      fb: "parmarchanchal867@gmail.com",
+      image: "ChanchalParmar.jpg",
+      },
+      {
+   
+      name: "Manvi",
+      cid: "22BA053",
+      fb: "manvikakkar85@gmail.com",
+      ln: "https://www.linkedin.com/in/manvi-kakkar-a36647256",
+      image: "ManviKakkar.jpg"
+      },
+      {
+      name: "Raghav garg",
+      cid: "22CE150",
+      fb: "raghavopgarg0512@gmail.com",
+      image: "RaghavGarg.jpg"
+      },
+  {
+    name: "Harshil ",
+    cid: "22CE128",
+    fb: "harshilarora40@gmail.com",
+    ln: "https://www.linkedin.com/in/harshil-arora-b2733a256",
+    image: "HarshilArora.jpg"
+    },
+    {
+    name: "Mohit",
+    cid: "22CE047",
+    fb: "gargmohit0104@gmail.com",
+    ln: "Mohit Garg",
+    image: "Mohit.jpg",
+    },
+    {
+      name: "Yash Bansal",
+      cid: "22BA085",
+      fb: "yash.bansal1810@gmail.com",
+      image: "YashBansal.jpg"
+      },
+      {
+        name: "Nancy ",
+        cid: "22CE139",
+        fb: "nancysharma77222@gmail.com",
+        image: "NANCYSHARMA.jpg",
+        },
+      {
+     name: "Divesh Kumar",
+     cid: "22EC009",
+     fb: "diveshpannu@gmail.com",
+     ln: "https://www.linkedin.com/in/divesh-pannu/",
+     image: "DiveshPannu.jpg",
+      },
+      {
+
+        name: "Ashish",
+        cid: "22EC012",
+        fb: "Parjapatiashish82@gmail.com",
+        image: "AshishParjapati.jpg"
+        },
+        {
+        name: "Sneha",
+        cid: "22BA042",
+        fb: "snehagandhi684@gmail.com",
+        image: "SnehaGandhi.jpg"
+        },
+        {
+          name: "Himanshu",
+          cid: "22ba091",
+          fb: "himanshusaini0138@gmail.com",
+          image: "HimanshuSaini.JPG"
+          },
+          {
+          
+          name: "Yogita",
+          cid: "22BA012",
+          fb: "yogitachail26@gmail.com",
+          image: "YogitaChail.jpg"
+          },
+          {
+            name: "Rajat Goyat",
+            cid: "22ce086",
+            fb: "rajatgoyat2004@gmail.com",
+            image: "RajatGoyat.jpg"
+            },
+          {
+          name: "Somay Sharma",
+          cid: "22MBA001",
+          fb: "somaysharma@protonmail.com",
+          image: "Somay.jpg"
+          },
+]
+  
+
+
+
+
